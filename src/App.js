9f236e929ffc0886e69e7070924b23536cf3644a
keshibat <kensukeshibata@gmail.com>
2020-10-19 16:46:43 +0900

    ftb

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Global, css } from '@emotion/core'
 import Navigation from './components/Navigation';
 
+const GlobalStyle = css`
+  body {
+        margin: 0;
+        padding: 0;
+        background: linear-gradient(90deg,rgba(92,45,215,1),rgba(52,135,248,1));
+       }
+`;
+
 function App() {
   return (
   <>
   <Global
-    styles={css`
-      body {
-              margin: 0;
-              padding: 0;
-              background: linear-gradient(90deg,rgba(92,45,215,1),rgba(52,135,248,1));
-            }
-          `}
+    styles={GlobalStyle}
     />
       <div>
         <Navigation/>
